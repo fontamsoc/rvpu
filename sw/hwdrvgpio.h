8a1cf36d4010be0b3a66e31aa6ab39a2a838dd48
William Tambe <tambewilliam@gmail.com>
2026-04-28 10:13:59 -0500

    hwdrvgpio: rework

--- sw/hwdrvgpio.h
+++ sw/hwdrvgpio.h
@@ -1,74 +1,87 @@
 // SPDX-License-Identifier: GPL-2.0-only
-// 20260420 (c) William Fonkou Tambe
+// 20260518 (c) William Fonkou Tambe
 
 #ifndef HWDRVGPIO_H
 #define HWDRVGPIO_H
 
 // Structure representing a GPIO device.
-// The field addr must be valid.
+// Initial state before use:
+// - The field addr must be valid.
+// - The field outval must be initial value.
 typedef struct {
-	// Device address.
-	void* addr;
-	// Count of IOs.
-	uintptr_t iocnt;
-	// Clock frequency in Hz used by the device.
-	uintptr_t clkfreq;
+	void* addr; // Device address.
+	uintptr_t outval; // Output value.
+	uintptr_t iocnt; // Count of IOs.
+	uintptr_t clkfreq; // Clock frequency in Hz used by the device.
 } hwdrvgpio_t;
-
-// Commands.
-#define HWDRVGPIO_CMDCONFIGUREIO 0
-#define HWDRVGPIO_CMDSETDEBOUNCE 1
 
 // Configure IOs where "arg" is a bitmap where each bit
 // 0/1 configures corresponding IO as an input/output.
-// dev->iocnt gets set to the IO count.
-static inline void hwdrvgpio_configureio (hwdrvgpio_t *dev, uintptr_t arg) {
-	void* addrDat = dev->addr;
+// dsc->iocnt gets set to the IO count.
+static inline void hwdrvgpio_configureio (hwdrvgpio_t *dsc, uintptr_t arg) {
+	void* addrDat = dsc->addr;
 	void* addrCmd = (addrDat + sizeof(uintptr_t));
 	uintptr_t dat;
 	do {
-		*(volatile uintptr_t *)addrCmd = ((arg<<1) | HWDRVGPIO_CMDCONFIGUREIO);
+		*(volatile uintptr_t *)addrCmd = ((arg << 1) | 0/*CMDCONFIGUREIO*/);
 		dat = *(volatile uintptr_t *)addrCmd;
-	} while ((dat & 0b1) != HWDRVGPIO_CMDCONFIGUREIO);
-	dev->iocnt = ((intptr_t)dat >> 1);
+	} while ((dat & 1) != 0/*CMDCONFIGUREIO*/);
+	dsc->iocnt = ((intptr_t)dat >> 1);
 }
 
 // Configure clockcycle count used to debounce applicable inputs.
-// dev->clkfreq gets set to the clock frequency in Hz used by the device.
-static inline void hwdrvgpio_setdebounce (hwdrvgpio_t *dev, uintptr_t arg) {
-	void* addrDat = dev->addr;
+// dsc->clkfreq gets set to the clock frequency in Hz used by the device.
+static inline void hwdrvgpio_setdebounce (hwdrvgpio_t *dsc, uintptr_t arg) {
+	void* addrDat = dsc->addr;
 	void* addrCmd = (addrDat + sizeof(uintptr_t));
 	uintptr_t dat;
 	do {
-		*(volatile uintptr_t *)addrCmd = ((arg<<1) | HWDRVGPIO_CMDSETDEBOUNCE);
+		*(volatile uintptr_t *)addrCmd = ((arg << 1) | 1/*CMDSETDEBOUNCE*/);
 		dat = *(volatile uintptr_t *)addrCmd;
-	} while ((dat & 0b1) != HWDRVGPIO_CMDSETDEBOUNCE);
-	dev->clkfreq = ((intptr_t)dat >> 1);
+	} while ((dat & 1) != 1/*CMDSETDEBOUNCE*/);
+	dsc->clkfreq = ((intptr_t)dat >> 1);
+}
+
+// Initialize outputs state.
+static inline void hwdrvgpio_ini (hwdrvgpio_t *dsc, uintptr_t arg) {
+	*(volatile uintptr_t *)dsc->addr = arg;
+	dsc->outval = arg;
 }
 
 // Set outputs.
-static inline void hwdrvgpio_set (hwdrvgpio_t *dev, uintptr_t arg) {
-	*(volatile uintptr_t *)dev->addr = arg;
+static inline void hwdrvgpio_set (hwdrvgpio_t *dsc, uintptr_t arg) {
+	arg |= dsc->outval;
+	if (arg ^ dsc->outval) { // Update only if there is a difference.
+		*(volatile uintptr_t *)dsc->addr = arg;
+		dsc->outval = arg;
+	}
 }
 
-// Andset outputs.
-static inline void hwdrvgpio_andset (hwdrvgpio_t *dev, uintptr_t arg) {
-	_atomic_and((volatile uintptr_t *)dev->addr, arg);
+// Clear outputs.
+static inline void hwdrvgpio_clr (hwdrvgpio_t *dsc, uintptr_t arg) {
+	arg = (dsc->outval & ~arg);
+	if (arg ^ dsc->outval) { // Update only if there is a difference.
+		*(volatile uintptr_t *)dsc->addr = arg;
+		dsc->outval = arg;
+	}
 }
 
-// Orset outputs.
-static inline void hwdrvgpio_orset (hwdrvgpio_t *dev, uintptr_t arg) {
-	_atomic_or((volatile uintptr_t *)dev->addr, arg);
-}
-
-// Xorset outputs.
-static inline void hwdrvgpio_xorset (hwdrvgpio_t *dev, uintptr_t arg) {
-	_atomic_xor((volatile uintptr_t *)dev->addr, arg);
+// Toggle outputs.
+static inline void hwdrvgpio_tgl (hwdrvgpio_t *dsc, uintptr_t arg) {
+	arg ^= dsc->outval;
+	*(volatile uintptr_t *)dsc->addr = arg;
+	dsc->outval = arg;
 }
 
 // Get inputs.
-static inline uintptr_t hwdrvgpio_get (hwdrvgpio_t *dev, uintptr_t arg) {
-	return *(volatile uintptr_t *)dev->addr;
+static inline uintptr_t hwdrvgpio_get (hwdrvgpio_t *dsc) {
+	return *(volatile uintptr_t *)dsc->addr;
 }
+
+#define BIT(n) (1UL << (n))
+#define GPIO_SET(dsc, pin) hwdrvgpio_set(dsc, BIT(pin))
+#define GPIO_CLR(dsc, pin) hwdrvgpio_clr(dsc, BIT(pin))
+#define GPIO_TGL(dsc, pin) hwdrvgpio_tgl(dsc, BIT(pin))
+#define GPIO_GET(dsc, pin) ((hwdrvgpio_get(dsc) >> (pin)) & 1)
 
 #endif /* HWDRVGPIO_H */
